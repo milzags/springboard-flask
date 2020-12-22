d34f34faab2d020b7b55047a25187ab7f2f379d6
milanzagorac <zagoracm@gmail.com>
2020-12-21 21:29:55 -0500

    more study notes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from random import randint
+from random import randint, choice
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
@@ -44,3 +44,15 @@ def show_lucky_num():
     """ example of simple dynamic template """
     num = randint(1,20)
     return render_template('lucky.html',lucky_num = num)
+
+@app.route('/form')
+def show_form():
+    return render_template('form.html')
+
+COMPLIMENTS = ['cool', 'clever', 'tenacious']
+
+@app.route('/greet')
+def greeting():
+    username = request.args['username']
+    nice_thing = choice(COMPLIMENTS)
+    return render_template('greet.html', username=username, compliment = nice_thing)
